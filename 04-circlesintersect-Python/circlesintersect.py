85b67caa2192dd26aa87cb5623dbc7a8cff8b5aa
KaloriRohit <kalorirohit@msitprogram.net>
2020-07-13 19:15:35 +0530

    Jul 13  19:15:35

--- 04-circlesintersect-Python/circlesintersect.py
+++ 04-circlesintersect-Python/circlesintersect.py
@@ -13,3 +13,10 @@ def fun_circlesintersect(x1, y1, r1, x2, y2, r2):
 	if total_radius == distance:
 		return True
 	return False 
+
+
+if __name__ == "__main__":
+	print(fun_circlesintersect(5, 6, 14, 8, 7, 9))
+	print(fun_circlesintersect(-10, 8, 30, 14, -24, 10))
+	print(fun_circlesintersect(3, 4, 5, 14, 18, 8))
+	print(fun_circlesintersect(2, 3, 12, 15, 28, 10))
