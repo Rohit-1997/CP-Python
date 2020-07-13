2e41ae38dc2472698dd0e0c9ea30c7b66ac0cbb1
KaloriRohit <kalorirohit@msitprogram.net>
2020-07-13 22:38:10 +0530

    Jul 13  22:38:10

--- 08-ismultiple-Python/ismultiple.py
+++ 08-ismultiple-Python/ismultiple.py
@@ -2,8 +2,5 @@
 # and returns True if m is a multiple of n and False otherwise. 
 # Note that 0 is a multiple of every integer including itself. 
 # Also, you should make constructive use of the isFactor function you just wrote above.
-
-
-
 def fun_ismultiple(m, n):
-	return False # replace with your solution
+	return m%n == 0
