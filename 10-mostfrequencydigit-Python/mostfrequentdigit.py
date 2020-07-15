90e0f068356a89d10a3585167a31bd16dd714f86
KaloriRohit <kalorirohit@msitprogram.net>
2020-07-15 23:37:53 +0530

    Jul 15  23:37:53

--- 10-mostfrequencydigit-Python/mostfrequentdigit.py
+++ 10-mostfrequencydigit-Python/mostfrequentdigit.py
@@ -6,8 +6,17 @@ from collections import defaultdict
 def mostfrequentdigit(n):
 	dict_data = defaultdict(int)
 	n = abs(n)
+	result = []
 
 	while n > 0:
 		ele = n % 10
 		dict_data[ele] += 1
 		n = n // 10
+	
+	max_value = max(dict_data.values())
+
+	for key in dict_data:
+		if dict_data[key] == max_value:
+			result.append(key)
+
+	return min(result)
