d5b6e2c427a21a2dcde40ee6bc4f336b9e8678e4
KaloriRohit <kalorirohit@msitprogram.net>
2020-07-15 15:39:46 +0530

    Jul 15  15:39:46

--- 07-set_kth_digit-Python/test_set_kth_digit.py
+++ 07-set_kth_digit-Python/test_set_kth_digit.py
@@ -6,7 +6,7 @@ import pytest
 
 @pytest.mark.parametrize('Number, k, digit, result',[
     (468, 0, 1, 461), (468, 1, 1, 418),
-    (468, 2, 1, 168), (468, 3, 1, 468),
+    (468, 2, 1, 168), (468, 3, 1, 1468),
     (-468, 3, 1, -1468)
 ])
 def test_fun_set_kth_digit(Number, k, digit, result):
