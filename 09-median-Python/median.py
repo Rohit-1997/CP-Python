4aa8e1cc99770ff76372bbb27e424ea4c9084c08
KaloriRohit <kalorirohit@msitprogram.net>
2020-07-16 16:08:12 +0530

    Jul 16  16:08:12

--- 09-median-Python/median.py
+++ 09-median-Python/median.py
@@ -12,9 +12,10 @@ def median(a):
 	if size%2 != 0:
 		return a[size//2]
 	mid = (size- 1)//2
+	print(mid)
 	return (a[mid - 1] + a[mid + 1]) / 2
 
 print(median([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
 print(median([1, 2, 3, 4, 5.5, 5.1, 7, 8, 9, 10]))
-print(median([1]))
-print(median([]))
+# print(median([1]))
+# print(median([]))
