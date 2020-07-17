32476ba9e86a55a51d9c3d3bfde9f24810886548
KaloriRohit <kalorirohit@msitprogram.net>
2020-07-17 22:09:14 +0530

    Jul 17  22:09:14

--- 10-isrotation-Python/isrotation.py
+++ 10-isrotation-Python/isrotation.py
@@ -5,6 +5,23 @@
 # 3412 is a rotation of 1234. Any number 
 # is a rotation of itself.
 
+from collections import defaultdict
+
 def isrotation(x, y):
-	# Your code goes here
-	pass
+	dict_data = defaultdict(int)
+	x = str(x)
+	y = str(y)
+	size_x = len(x)
+	for i in range(len(x)):
+		index = (i+1) % size_x 
+		dict_data[x[i]] = x[index]
+	print(dict_data)
+
+	for i in range(len(y) - 1):
+		if dict_data[y[i]] != y[i+1]:
+			return False
+	return True
+
+print(isrotation(3412, 1234))
+
+	
