1f48d5e42cf4cae02f64027ab340f3da9be88520
KaloriRohit <kalorirohit@msitprogram.net>
2020-07-18 20:14:49 +0530

    Jul 18  20:14:49

--- 04-getaverage-Python/getaverage.py
+++ 04-getaverage-Python/getaverage.py
@@ -6,9 +6,15 @@
 # For example, getAverage('13,excused,14,absent') ignores the two 
 # strings and averages 13 and 14 to return 13.5. Also, getAverage('a,b,c') returns 0.
 
-
-
-
 def fun_getaverage(s): 
-	return 0.0
+	list_data = s.split(',')
+	number_list = []
+
+	for ele in list_data:
+		try:
+			number = int(ele)
+			number_list.append(number)
+		except:
+			continue
+	return sum(number_list) / len(number_list)
 
