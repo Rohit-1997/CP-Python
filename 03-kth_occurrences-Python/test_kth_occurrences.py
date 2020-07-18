fc8e8859c6699a66c2cc4eb42ace6453ebe3798e
KaloriRohit <kalorirohit@msitprogram.net>
2020-07-18 19:39:00 +0530

    Jul 18  19:39:00

--- 03-kth_occurrences-Python/test_kth_occurrences.py
+++ 03-kth_occurrences-Python/test_kth_occurrences.py
@@ -7,7 +7,7 @@ import pytest
 @pytest.mark.parametrize('s1, s2, result',[	
 
 ("helllo woorld", 2, 'o'), ("hsdibdouvbeiovbfeibvdsioubidsbvviefadbcildfab", 4, 'v'),
-("    h  ", 1, 'h'), ("hello hyderabad a", 1, 'a'), ("england", 1, 'n'),
+("    h  ", 1, 'h'), ("hello hyderabad a", 1, 'a'), ("england", 1, 'd'),
 ("asuszenphonemaxm1 aemnsh", 6, 'h')
 
 ])
