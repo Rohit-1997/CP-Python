0b0f73ee63c5746e5db36b7421a88f3e8b6fc1a5
KaloriRohit <kalorirohit@msitprogram.net>
2020-07-18 17:19:28 +0530

    Jul 18  17:19:28

--- 01-rotatestrings-Python/rotatestrings.py
+++ 01-rotatestrings-Python/rotatestrings.py
@@ -14,14 +14,14 @@ def fun_rotatestrings(s, n):
 
 	if abs(n) == n:
 		print("In here")
-		for i in range(n):
+		for i in range(size):
 			index = (i-1) % size
 			print(index)
 			aux_x[index] = s[i]
 		print(aux_x)
 		return "".join(aux_x)
 	else:
-		for i in range(n):
+		for i in range(size):
 			index = (i+1) % size
 			print(index)
 			aux_x[index] = s[i]
