f49e2c58bb50461f2a1d8a439ca8fb8a59779b35
KaloriRohit <kalorirohit@msitprogram.net>
2020-07-23 14:54:20 +0530

    Jul 23  14:54:20

--- 03-nth_lefttruncatableprime-Python/nth_lefttruncatableprime.py
+++ 03-nth_lefttruncatableprime-Python/nth_lefttruncatableprime.py
@@ -9,7 +9,7 @@ import math
 def is_prime(number):
     if number <= 1:
         return False
-    for i in range(2,int(math.sqrt(number))):
+    for i in range(2,number):
         if number % i == 0:
             return False
     return True
