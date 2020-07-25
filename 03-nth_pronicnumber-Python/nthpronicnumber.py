1d32c8e586a2931645226a6ce0a26e289eecf7b2
KaloriRohit <kalorirohit@msitprogram.net>
2020-07-25 15:57:44 +0530

    Jul 25  15:57:44

--- 03-nth_pronicnumber-Python/nthpronicnumber.py
+++ 03-nth_pronicnumber-Python/nthpronicnumber.py
@@ -4,5 +4,16 @@
 # number n is a product of x and (x+1).
 
 def nthpronicnumber(n):
-	# Your code goes here
-	pass
+	if n == 0:
+		return 0
+	prev_number = 1
+	current_number = 2
+	result = 0
+	counter = 0
+
+	while counter < n:
+		result = prev_number * current_number
+		counter += 1
+	return result
+
+print(nthpronicnumber(2))
